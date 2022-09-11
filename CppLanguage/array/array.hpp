e36f114f861aba5753f9dcc65a39b2e01fab53f0
ArtemiyKotsyubenko <kotsiubenko.aa@phystech.edu>
2022-09-12 00:43:25 +0300

    Add array task

--- CppLanguage/array/array.hpp
+++ CppLanguage/array/array.hpp
@@ -1,17 +1,28 @@
-#pragma once
-// NOLINT
-
 #include <stdexcept>
 
 template <typename T, int N>
 class Array {
  public:
+  Array() = default;
+
+  Array(std::initializer_list<T> lst) {  // Do not touch.
+    std::copy(lst.begin(), lst.end(), arr_);
+  }
+
   T& Front();
+
   T& Back();
+
   int Size();
+
   bool Empty();
+
   T* Data();
+
   T& operator[](int i);
-  
+
   void Swap(Array<T, N>& cur);
+
+ private:
+  T arr_[N];
 };
--- CppLanguage/array/test_array.cpp
+++ CppLanguage/array/test_array.cpp
@@ -2,4 +2,55 @@
 #include <catch.hpp>
 
 #include "array.hpp"
+#include "array.hpp"  // Check Guards or pragma.
 
+TEST_CASE("Array tests") {
+  Array<int, 4> arr{1, 2, 3, 4};  // Constructor from init-list
+
+  SECTION("Check object after instantiation") {
+    static_assert(sizeof(arr) == sizeof(int) * 4);  // Compile-time check.
+    REQUIRE(arr.Size() == 4);
+    REQUIRE_FALSE(arr.Empty());
+  }
+
+  SECTION("Check operator[]") {
+    REQUIRE(arr[0] == 1);
+    arr[1] = 0;
+    REQUIRE(arr[1] == 0);
+  }
+
+  SECTION("Check data access methods methods") {
+    REQUIRE(arr.Front() == 1);
+    REQUIRE(arr.Back() == 4);
+    REQUIRE(arr.Data() == &arr[0]);
+  }
+
+  SECTION("Check that Front, Back methods returns reference") {
+    arr.Front() = -1;
+    REQUIRE(arr.Front() == -1);
+    arr.Back() = -1;
+    REQUIRE(arr.Back() == -1);
+  }
+
+  SECTION("Check Swap") {
+    Array<int, 4> other{0, 0, 0, 0};
+    other.Swap(arr);
+    REQUIRE(other[0] == 1);
+    REQUIRE(other[1] == 2);
+    REQUIRE(other[2] == 3);
+    REQUIRE(other[3] == 4);
+
+    REQUIRE(arr[0] == 0);
+    REQUIRE(arr[1] == 0);
+    REQUIRE(arr[2] == 0);
+    REQUIRE(arr[3] == 0);
+  }
+
+  SECTION("Const methods") {  // Assertions are the same, but code won't compile in case of absence const qualifiers
+    const Array<int, 4>& const_ref = arr;
+    REQUIRE(arr[0] == 1);
+    REQUIRE(arr.Front() == 1);
+    REQUIRE(arr.Back() == 4);
+    REQUIRE(arr.Data() == &arr[0]);
+  }
+}
