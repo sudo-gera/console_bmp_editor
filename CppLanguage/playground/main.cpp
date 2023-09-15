b442d784b1af764c9ebaaaec38c74f9cca4b068d
sudo-gera <gera.tatarinov.999@gmail.com>
2023-09-15 04:52:34 +0300

    cleanup

--- CppLanguage/playground/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <cstdlib>
-
-int main() {
-  return 0;
-}
--- CppLanguage/tasks/complex/abs.hpp
+++
@@ -1,9 +0,0 @@
-
-#include "complex.hpp"
-#include <cmath>
-
-/*
-???  Abs(??? number) {
-  return number > 0 ? number : -number;
-}
-*/
